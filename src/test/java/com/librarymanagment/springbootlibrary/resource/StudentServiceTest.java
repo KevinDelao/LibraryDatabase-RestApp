e9d5d9e3cae96fed8a70a8f91fb5e2051bdb9a18
KevinDelao <delao14x@gmail.com>
2021-09-24 12:50:50 -0700

    new test

---
+++ src/test/java/com/librarymanagment/springbootlibrary/resource/StudentServiceTest.java
@@ -0,0 +1,32 @@
+package com.librarymanagment.springbootlibrary.resource;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void findStudent() {
+    }
+
+    @Test
+    void ifNotBorrowingBook() {
+    }
+
+    @Test
+    void ifStudentExists() {
+    }
+
+    @Test
+    void deleteStudentByID() {
+    }
+
+    @Test
+    void save() {
+    }
+}
